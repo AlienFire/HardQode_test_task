c11541860afb66e76d13bd6d0e191bca992cb7a5
Ogneva.A.C <ognevaalena22@gmail.com>
2024-03-01 12:20:40 +0300

    Добавлены модели таблиц

--- hard_qode/edu_platform/models.py
+++ hard_qode/edu_platform/models.py
@@ -1,4 +1,26 @@
 from django.db import models
 
 
+class Product(models.Model):
+    creator = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    start_at = models.DateTimeField()
+    price = models.IntegerField()
+    student_count_from = models.IntegerField(default=0)
+    student_count_to = models.IntegerField()
 
+
+class Lesson(models.Model):
+    link = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Students(models.Model):
+    name = models.CharField(max_length=100)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
